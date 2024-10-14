ac8bf70b5459fed4bea6384ec52287628680dd16
Tholumuzi Khuboni <contact@tholumuzi.co.za>
2024-10-14 12:57:18 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -23,10 +23,10 @@ const sliderFunc = function (count) {
   slidesImg.src = `./images/food-${count}.png`;
 }
 next.addEventListener('click', function () {
-  count = count >= 3 ? 0 : count + 1; // count = count + 1
+  count = count >= 4 ? 0 : count + 1; // count = count + 1
   sliderFunc(count);
 });
 prev.addEventListener('click', function () {
-  count = count <= 0 ? 3 : count - 1; // count = count - 1
+  count = count <= 0 ? 4 : count - 1; // count = count - 1
   sliderFunc(count);
 });
